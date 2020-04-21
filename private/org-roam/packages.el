e8fcc36e32b84564e463a2641f385b7e87895c10
Sreejith Pp <1743700+ppsreejith@users.noreply.github.com>
2020-04-21 06:36:18 +0530

    Update roam config to keep it clear

--- private/org-roam/packages.el
+++ private/org-roam/packages.el
@@ -1,6 +1,8 @@
 (defconst org-roam-packages
-  '((org-roam :location
-              (recipe :fetcher github :repo "jethrokuan/org-roam" :branch "develop"))))
+  ;; '((org-roam :location
+  ;;             (recipe :fetcher github :repo "jethrokuan/org-roam" :branch "develop"))))
+  '(org-roam)
+  )
 
 (defun org-roam/init-org-roam ()
   (use-package org-roam
@@ -20,7 +22,7 @@
       (spacemacs/declare-prefix-for-mode 'org-mode "mr" "org-roam")
       (spacemacs/set-leader-keys-for-major-mode 'org-mode
         "rl" 'org-roam
-        "rt" 'org-roam-today
+        "rt" 'org-roam-dailies-today
         "rb" 'org-roam-switch-to-buffer
         "rf" 'org-roam-find-file
         "ri" 'org-roam-insert
